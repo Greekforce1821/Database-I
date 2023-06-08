6d638502fac850cd41475af58352302fc495dca9
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2023-06-08 15:04:31 +0300

    modified the index.html, etc.
    modified the index.html file and added a search engine for the show_flights.....php file to receive an extra bonus on my grade :P

--- show_flights_from_athens_to_corfu.php
+++ show_flights_from_athens_to_corfu.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset='utf-8'>
-    <title>Πτήσεις που αναχωρούν την Τρίτη 13/06/2023 από την Αθήνα για Κέρκυρα</title>    
+    <title>Ποιες είναι οι πτήσεις που αναχωρούν την Τρίτη 13/06/2023 από την Αθήνα για Κέρκυρα;</title>    
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -17,6 +17,10 @@
             text-align: center;
         }
 
+        ::placeholder {
+            text-align: center;
+        }
+        
         .logo-image {
             position: absolute;
             top: 1px;
@@ -31,13 +35,25 @@
             width: 100px;
         }
 
-        footer {
+        .search-form {
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            text-align: center;
+            margin-top: 20px;
             background-color: #f2f2f2;
+        }
+
+        .search-input {
+            width: 300px;
             padding: 10px;
             text-align: center;
-            position: absolute;
-            bottom: 0;
-            width: 100%;
+            font-size: 16px;
+        }
+
+        .search-button {
+            padding: 10px 20px;
+            font-size: 16px;
         }
 
         .grid-container {
@@ -58,6 +74,15 @@
             text-align: center;
             margin-top: 20px;
         }
+
+        footer {
+            background-color: #f2f2f2;
+            padding: 10px;
+            text-align: center;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
     </style>
 </head>
 <body>
@@ -65,76 +90,91 @@
         <h1>Airport DB User Group III</h1>
     </header>
 
+    <h2 style="text-align: center;">Ποιες είναι οι πτήσεις που αναχωρούν την Τρίτη 13/06/2023 από την Αθήνα για Κέρκυρα;</h2>
+
     <nav>
-        <a href="https://ionio.gr" target="_blank"><img class="logo-image" src="images/uni-logo.png" alt="Logo"></a>
-        <a href="https://di.ionio.gr" target="_blank"><img class="right-logo-image" src="images/di.jpg" alt="DI Logo" style="width: 300px;"></a>
+        <form class="search-form" method="POST" action="">
+            <a href="https://ionio.gr" target="_blank"><img class="logo-image" src="images/uni-logo.png" alt="Logo"></a>
+            <a href="https://di.ionio.gr" target="_blank"><img class="right-logo-image" src="images/di.jpg" alt="DI Logo" style="width: 300px;"></a>
+            <br><br>
+            <input type="text" id="origin" name="origin" class="search-input" placeholder="Origin -> Athens" required>
+            <br><br>
+            <input type="text" id="destination" name="destination" class="search-input" placeholder="Destination -> Corfu" required>
+            <br><br>
+            <input type="date" id="date" name="date" class="search-input" placeholder="Ημερομηνία" required>
+            <br><br>
+            <button type="submit" class="search-button">Αναζήτηση</button>
+        </form>
     </nav>
 
-    <h2 style="text-align: center;">Ποιες πτήσεις αναχωρούν την Τρίτη 13/06/2023 από Αθήνα για Κέρκυρα;</h2>
+    <?php
 
-<?php
-    // include the file with DB connection
     include 'connDB.php';
 
     if ($conn->connect_error) {
         die('Σφάλμα κατά τη σύνδεση με τη βάση δεδομένων: ' . $conn->connect_error);
     }
 
-    // SQL query to execute
-    $sql = "SELECT flights.flight_number, flights.date, gates.gate_name 
-            FROM flights 
-            INNER JOIN gates ON flights.gate_id = gates.gates_id 
-            WHERE flights.origin = 'Athens' AND flights.destination = 'Corfu' AND flights.date = '2023-06-13' ";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $origin = $_POST["origin"];
+        $destination = $_POST["destination"];
+        $date = $_POST["date"];
 
-    // Execute the query and get the results
-    $result = $conn->query($sql);
 
-    // Check if there are any results
-    if ($result->num_rows > 0) {
-        // Create an array to store the results
-        $flights = array();
+        $sql = "SELECT flights.flight_number, flights.date, gates.gate_name 
+                FROM flights 
+                INNER JOIN gates ON flights.gate_id = gates.gates_id 
+                WHERE flights.origin = '$origin' AND flights.destination = '$destination' AND flights.date = '$date' ";
 
-        // Fetch each row and add it to the flights array
-        while ($row = $result->fetch_assoc()) {
-            $flights[] = $row;
+
+        $result = $conn->query($sql);
+
+
+        if ($result->num_rows > 0) {
+
+            $flights = array();
+
+            
+            while ($row = $result->fetch_assoc()) {
+                $flights[] = $row;
+            }
+
+            
+            echo '<div class="grid-container">';
+
+            
+            foreach ($flights as $flight) {
+                $flight_number = $flight['flight_number'];
+                $date = $flight['date'];
+                $gate_name = $flight['gate_name'];
+
+              
+                echo '<div class="grid-item">
+                        <p>Αριθμός πτήσης: ' . $flight_number . '</p>
+                        <p>Ημερομηνία αναχώρησης: ' . $date . '</p>
+                        <p>Πύλη εξόδου: ' . $gate_name . '</p>
+                    </div>';
+            }
+
+            
+            echo '</div>';
+        } else {
+            
+            echo '<p> Δεν βρέθηκαν εγγραφές για τις παραπάνω πτήσεις 😞 </p>';
         }
-
-        // Print the grid container opening tag
-        echo '<div class="grid-container">';
-
-        // Iterate over the flights array and display each flight
-        foreach ($flights as $flight) {
-            $flight_number = $flight['flight_number'];
-            $date = $flight['date'];
-            $gate_name = $flight['gate_name'];
-
-            // Print each grid item
-            echo '<div class="grid-item">
-                    <p>Αριθμός πτήσης: ' . $flight_number . '</p>
-                    <p>Ημερομηνία αναχώρησης: ' . $date . '</p>
-                    <p>Πύλη εξόδου: ' . $gate_name . '</p>
-                </div>';
-        }
-
-        // Print the grid container closing tag
-        echo '</div>';
-
-        // Print the return link
-        echo '<a href="index.html" class="return-link">Επιστροφή στην αρχική σελίδα</a>';
-    } else {
-        // In case there are no records in the result set...
-        echo '<p> Δεν βρέθηκαν εγγραφές για τις παραπάνω πτήσεις 😞 </p>';
-
-        // Print the return link
-        echo '<a href="index.html" class="return-link">Επιστροφή στην αρχική σελίδα</a>';
     }
 
-    // Close the connection to the database
+    
     $conn->close();
-?>
+    ?>
+
+   
+    <div class="return-link">
+        <a href="index.html">Επιστροφή στην αρχική σελίδα</a>
+    </div>
 
     <footer>
-        <p>Ιόνιο Πανεπιστήμιο | Τμήμα Πληροφορικής | Μάθημα: Bάσεις Δεδομένων Ι | Ακαδημαϊκό έτος: 2022 - 2023 | Κέρκυρα</p>
+        <p>Ιόνιο Πανεπιστήμιο | Τμήμα Πληροφορικής | Μάθημα: Βάσεις Δεδομένων Ι | Ακαδημαϊκό έτος: 2022 - 2023 | Κέρκυρα</p>
     </footer>
 
 </body>
